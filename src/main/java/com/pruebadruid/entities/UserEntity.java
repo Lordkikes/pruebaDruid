76c5edf66af041da736d5a37d10f298ef7d40895
Enrique <enrique.navarro@ingenieros.com>
2022-08-11 14:38:13 +0200

    Create UserEntity and UserRepository class

---
+++ src/main/java/com/pruebadruid/entities/UserEntity.java
@@ -0,0 +1,32 @@
+package com.pruebadruid.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String password;
+
+    private Date birthDate;
+
+
+}
