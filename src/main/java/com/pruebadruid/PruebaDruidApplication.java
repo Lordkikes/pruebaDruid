c515760e641552c5f45e33c3c5277f4240a53cbe
Enrique <enrique.navarro@ingenieros.com>
2022-08-11 11:13:26 +0200

    Create Project

---
+++ src/main/java/com/pruebadruid/PruebaDruidApplication.java
@@ -0,0 +1,13 @@
+package com.pruebadruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaDruidApplication.class, args);
+    }
+
+}
